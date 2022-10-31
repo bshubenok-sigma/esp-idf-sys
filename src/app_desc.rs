039b268311fb394cf3b7b8c6f14e552c10207280
Bohdan Shubenok <bohdan.shubenok@sigma.software>
2022-10-31 19:15:45 +0200

    Add custom application descriptor

--- src/app_desc.rs
+++ src/app_desc.rs
@@ -43,3 +43,117 @@ macro_rules! esp_app_desc {
         };
     };
 }
+
+/// Macro to allocate memory in firmware file for custom application descriptor
+/// This data will be filled with 0xFF and expected to be filled with usefull data from
+#[macro_export]
+macro_rules! esp_custom_desc_placeholder {
+    ($length: expr) => {
+        #[no_mangle]
+        #[used]
+        #[link_section = ".rodata_custom_desc"]
+        #[allow(non_upper_case_globals)]
+        pub static esp_custom_desc_placeholder: [$crate::c_types::c_char; $length] =
+            [0xFF; $length];
+    };
+}
+
+
+///
+/// #[derive(Copy, Clone)]
+/// pub struct Sometype {
+///     a: u32,
+///     b: u32,
+///     c: u8,
+/// }
+/// sys::esp_custom_desc!(1024, Sometype);
+///
+#[macro_export]
+macro_rules! esp_custom_desc {
+    ($length: expr, $internal_type: ty) => {
+        #[repr(packed)]
+        #[derive(Copy, Clone)]
+        pub struct CustomAppDescriptorHeader {
+            pub magic: u32,
+            pub length: u32,
+        }
+
+        #[repr(packed)]
+        #[derive(Copy, Clone)]
+        pub struct CustomAppDescriptorData {
+            pub header: CustomAppDescriptorHeader,
+            pub internal: $internal_type,
+        }
+
+        #[repr(packed)]
+        pub union CustomAppDescriptor {
+            storage: [$crate::c_types::c_char; $length],
+            data: CustomAppDescriptorData,
+        }
+
+        impl CustomAppDescriptor {
+
+            pub fn load_data(&mut self) -> Result<(), $crate::EspError> {
+                unsafe {
+                    let partition = $crate::esp_ota_get_running_partition();
+
+                    if !partition.is_null() {
+                        let mut header: CustomAppDescriptorHeader = CustomAppDescriptorHeader {
+                            magic: 0,
+                            length: 0,
+                        };
+                        let err = $crate::esp_partition_read(
+                            partition,
+                            (std::mem::size_of::<$crate::esp_image_header_t>()
+                                + std::mem::size_of::<$crate::esp_image_segment_header_t>()
+                                + std::mem::size_of::<$crate::esp_app_desc_t>())
+                                as u32,
+                            (&mut header as *mut CustomAppDescriptorHeader)
+                                as *mut $crate::c_types::c_void,
+                            std::mem::size_of::<CustomAppDescriptorHeader>() as u32,
+                        );
+
+                        if err != $crate::ESP_OK {
+                            return $crate::esp!(err);
+                        }
+
+                        if header.magic == 0x78563412 {
+                            if (header.length <= std::mem::size_of::<CustomAppDescriptorHeader>() as u32) {
+                                return $crate::esp!($crate::ESP_ERR_INVALID_SIZE);
+                            }
+                            let err = $crate::esp_partition_read(
+                                partition,
+                                (std::mem::size_of::<$crate::esp_image_header_t>()
+                                    + std::mem::size_of::<$crate::esp_image_segment_header_t>()
+                                    + std::mem::size_of::<$crate::esp_app_desc_t>())
+                                    as u32,
+                                (self as *mut CustomAppDescriptor) as *mut $crate::c_types::c_void,
+                                std::mem::size_of::<CustomAppDescriptorHeader>() as u32,
+                            );
+
+                            if err != $crate::ESP_OK {
+                                return $crate::esp!(err);
+                            }
+                        }
+                    }
+
+                    Ok(())
+                }
+            }
+        }
+
+        #[no_mangle]
+        #[used]
+        #[link_section = ".rodata_custom_desc"]
+        #[allow(non_upper_case_globals)]
+        pub static esp_custom_desc: CustomAppDescriptor = {
+            assert!(
+                $length >= std::mem::size_of::<$internal_type>(),
+                "Allocated length is not enough for internal type"
+            );
+            CustomAppDescriptor {
+                storage: [0xEE; $length],
+            }
+        };
+    };
+}
